7d21c60ee0449b0cd25fee945948ffb806da92b7
wawo123 <clo7er4444@gmail.com>
2024-07-26 22:36:53 +0900

    Update VocaListEntity.java

--- vocatest/src/main/java/com/example/vocatest/entity/VocaListEntity.java
+++ vocatest/src/main/java/com/example/vocatest/entity/VocaListEntity.java
@@ -17,19 +17,26 @@ public class VocaListEntity { //단어장 목록
 
     private String title; // 단어장의 제목
 
-    //private int secret; // 0이면 비공개 1이면 공개
+    private int secret; // 0이면 비공개 1이면 공개
 
-    //private Long key; // 단어장 고유의 키값
-
+    private Long count; // 받아간 횟수
 
     // 아래부턴 생성자
     public VocaListEntity() {
 
     }
 
-    public VocaListEntity(String author, String title) {
+
+    
+    public VocaListEntity(String author, String title, int secret, Long count) {
         this.author = author;
         this.title = title;
+        this.secret = secret;
+        this.count = count;
+    }
+
+    public void addCount(Long count){
+        this.count =+ count;
     }
 
 }
