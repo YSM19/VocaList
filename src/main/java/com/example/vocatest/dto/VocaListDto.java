c9ccb508299e3e43a276a3f085680fb8f86d5e23
ysm <clov7153@gmail.com>
2024-08-03 00:24:36 +0900

    Change Directory

---
+++ src/main/java/com/example/vocatest/dto/VocaListDto.java
@@ -0,0 +1,21 @@
+package com.example.vocatest.dto;
+
+import com.example.vocatest.entity.VocaListEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VocaListDto { 
+    private String author;
+    private String title;
+
+    /*
+    public VocaListEntity toEntity(){
+        return new VocaListEntity(author, title); 
+    }
+    */
+        public VocaListEntity createVocalistToEntity(String email){
+        return new VocaListEntity(email, title, 0, 0L);
+    }
+}
