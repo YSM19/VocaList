3eb3e8560f36983138e6e03a684c2b6d86acf7b1
ysm <clov7153@gmail.com>
2024-09-22 18:47:08 +0900

    ci/cd testing

--- src/main/java/com/example/vocatest/VocatestApplication.java
+++ src/main/java/com/example/vocatest/VocatestApplication.java
@@ -9,6 +9,6 @@ public class VocatestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VocatestApplication.class, args);
-	} // ci/cd test11
+	} // ci/cd test 12
 
 }
