3f22ff8ceb50554fb175b7f4fddf049690b5ac39
ysm <clov7153@gmail.com>
2024-09-24 02:05:44 +0900

    bug fix

--- src/main/java/com/example/vocatest/controller/LogoutController.java
+++ src/main/java/com/example/vocatest/controller/LogoutController.java
@@ -3,6 +3,7 @@ package com.example.vocatest.controller;
 import com.example.vocatest.controllerDocs.LogoutControllerDocs;
 import com.example.vocatest.dto.CustomOAuth2User;
 import com.example.vocatest.jwt.JwtUtil;
+import com.example.vocatest.redis_change.RedisService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,25 +17,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
-public class LogoutController implements LogoutControllerDocs {
+public class LogoutController {
 
     private final JwtUtil jwtUtil;
+    private final RedisService redisService;
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
 
-        Cookie cookie = new Cookie("Authorization", null);
-        cookie.setMaxAge(0);     // 쿠키가 살아있을 시간
-        cookie.setPath("/");            // 쿠키가 전역에서 동작
-        cookie.setHttpOnly(true);       // http에서만 쿠키가 동작할 수 있도록 (js와 같은곳에서 가져갈 수 없도록)
+        Optional<Cookie> refreshCookie = Arrays.stream(cookies)
+                .filter(cookie -> "refresh".equals(cookie.getName()))
+                .findFirst();
 
+        if(!refreshCookie.isPresent()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String refreshToken = refreshCookie.get().getValue();
+        if(refreshToken == null || refreshToken.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String key = jwtUtil.getUsername(refreshToken);
+
+        if(redisService.getValue(key) == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        redisService.deleteValues(key);
+
+        Cookie cookie = new Cookie("refresh", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+
+        response.setStatus(HttpServletResponse.SC_OK);
         response.addCookie(cookie);
-
-        return ResponseEntity.ok("Logged out successfully");
     }
+
 }
