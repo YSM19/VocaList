31d703a2aebd47fdaf45dad106667a907e227d81
ysm <clov7153@gmail.com>
2024-09-30 22:04:44 +0900

    http

--- src/main/java/com/example/vocatest/config/SecurityConfig.java
+++ src/main/java/com/example/vocatest/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 
         http
                 .oauth2Login((oauth2) -> oauth2
-                        .loginPage("/login")
+//                        .loginPage("/login")
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(userService))
                         .successHandler(customSuccessHandler)
