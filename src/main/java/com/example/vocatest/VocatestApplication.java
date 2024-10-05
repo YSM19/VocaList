f8eb7dbe4a6d1079ac2363cd2c1cf1e9e37beaf9
ysm <clov7153@gmail.com>
2024-10-05 23:35:36 +0900

    http

--- src/main/java/com/example/vocatest/VocatestApplication.java
+++ src/main/java/com/example/vocatest/VocatestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication
 public class VocatestApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(VocatestApplication.class, args);
 	}
