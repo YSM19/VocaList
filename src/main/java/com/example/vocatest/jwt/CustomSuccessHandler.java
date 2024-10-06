5dafa81134f27771adb38c7e7b09e2d63cfd66e2
ysm <clov7153@gmail.com>
2024-10-06 15:57:14 +0900

    http

--- src/main/java/com/example/vocatest/jwt/CustomSuccessHandler.java
+++ src/main/java/com/example/vocatest/jwt/CustomSuccessHandler.java
@@ -69,7 +69,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         response.setStatus(HttpStatus.OK.value());
         //*aws*
 //        response.sendRedirect(frontUrl);      // 로그인 성공시 프론트에 알려줄 redirect 경로
-        response.sendRedirect("http://ec2-15-164-103-179.ap-northeast-2.compute.amazonaws.com:3000/auth-success");
+        response.sendRedirect("http://ec2-15-164-103-179.ap-northeast-2.compute.amazonaws.com:3000");
         // */
 
     }
