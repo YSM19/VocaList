c4e3c5e3628828b6ac60c04e625bdc1270e0fdf8
ysm <clov7153@gmail.com>
2024-10-15 22:09:02 +0900

    bug

--- src/main/java/com/example/vocatest/config/CorsMvcConfig.java
+++ src/main/java/com/example/vocatest/config/CorsMvcConfig.java
@@ -2,6 +2,7 @@ package com.example.vocatest.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,14 +14,26 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-
         corsRegistry.addMapping("/**")
-                .allowedOrigins(frontUrl)
-//                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .maxAge(3600)
                 .allowedOriginPatterns("*")
-                .allowCredentials(true);
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.TRACE.name()
+                );
     }
+//        corsRegistry.addMapping("/**")
+//                .allowedOrigins(frontUrl)
+////                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .maxAge(3600)
+//                .allowCredentials(true);
+//    }
 }
