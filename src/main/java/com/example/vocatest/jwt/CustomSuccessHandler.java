3936b52c3ef8b43a2ddb43d4884e618f616531e7
ysm <clov7153@gmail.com>
2024-10-15 18:39:08 +0900

    bug

--- src/main/java/com/example/vocatest/jwt/CustomSuccessHandler.java
+++ src/main/java/com/example/vocatest/jwt/CustomSuccessHandler.java
@@ -95,7 +95,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24*60*60);     // 쿠키가 살아있을 시간
-        cookie.setSecure(true);         // https에서만 동작할것인지 (로컬은 http 환경이라 안먹음)
+//        cookie.setSecure(true);         // https에서만 동작할것인지 (로컬은 http 환경이라 안먹음)
         cookie.setPath("/");            // 쿠키가 전역에서 동작
         cookie.setHttpOnly(true);       // http에서만 쿠키가 동작할 수 있도록 (js와 같은곳에서 가져갈 수 없도록)
 
