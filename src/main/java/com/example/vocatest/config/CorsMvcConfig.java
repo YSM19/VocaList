077dbbd8953235c868ec1cf05f3bdaedf2394e8a
ysm <clov7153@gmail.com>
2024-10-15 22:54:52 +0900

    bug

--- src/main/java/com/example/vocatest/config/CorsMvcConfig.java
+++ src/main/java/com/example/vocatest/config/CorsMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
-
+//
     @Value("${config.front_url}")
     private String frontUrl;
 
