8052242f46408daa952bad724d0ae209bd935f89
ysm <clov7153@gmail.com>
2024-10-16 23:01:16 +0900

    https swagger

--- src/main/java/com/example/vocatest/config/SecurityConfig.java
+++ src/main/java/com/example/vocatest/config/SecurityConfig.java
@@ -68,7 +68,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
 //                        .requestMatchers("/", "/reissue").permitAll()
                         .requestMatchers("/", "/login", "/reissue",
-                                "/swagger-ui/**", "/swagger-resources/**",
                                 "/api/vocalist/showall", "/api/vocalist/show/{vocalistId}",
                                 "/api/vocacontent/showall/{vocalistId}").permitAll()
                         .anyRequest().authenticated());
