608a589b2e7a791b54e7a7d76f672016e8bde1f5
ysm <clov7153@gmail.com>
2024-10-25 00:51:12 +0900

    N+1 문제

--- src/main/java/com/example/vocatest/repository/VocaContentRepository.java
+++ src/main/java/com/example/vocatest/repository/VocaContentRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface VocaContentRepository extends JpaRepository<VocaContentEntity, Long> {
-    @Query("SELECT vc from VocaContentEntity vc JOIN fetch vc.vocaListEntity WHERE vc.vocaListEntity.id = :vocaListId")
+    @Query("SELECT DISTINCT uv FROM UserVocaListEntity uv " +
+            "JOIN FETCH uv.vocaListEntity v " +
+            "JOIN FETCH uv.userEntity u " +
+            "JOIN FETCH VocaContentEntity vc ON vc.vocaListEntity.id = v.id " +  // VocaContentEntity를 패치 조인
+            "WHERE uv.userEntity.email = :email")
     List<VocaContentEntity> findByVocaListEntityId(Long vocaListId);
 }
