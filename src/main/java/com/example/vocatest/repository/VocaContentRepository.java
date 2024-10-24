d4f695ee3c6cee74f367df793578f533c76dc658
ysm <clov7153@gmail.com>
2024-10-25 00:08:51 +0900

    N+1 문제

--- src/main/java/com/example/vocatest/repository/VocaContentRepository.java
+++ src/main/java/com/example/vocatest/repository/VocaContentRepository.java
@@ -3,11 +3,11 @@ package com.example.vocatest.repository;
 import com.example.vocatest.entity.VocaContentEntity;
 import com.example.vocatest.entity.VocaListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface VocaContentRepository extends JpaRepository<VocaContentEntity, Long> {
+    @Query("SELECT vc from VocaContentEntity vc JOIN fetch vc.vocaListEntity WHERE vc.vocaListEntity.id = :vocaListId")
     List<VocaContentEntity> findByVocaListEntityId(Long vocaListId);
-
-
 }
