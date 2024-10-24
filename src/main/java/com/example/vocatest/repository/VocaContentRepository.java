bff78b360a32988c79dd03b917bb3b458230e6cb
ysm <clov7153@gmail.com>
2024-10-25 01:28:42 +0900

    N+1문제

--- src/main/java/com/example/vocatest/repository/VocaContentRepository.java
+++ src/main/java/com/example/vocatest/repository/VocaContentRepository.java
@@ -8,10 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface VocaContentRepository extends JpaRepository<VocaContentEntity, Long> {
-    @Query("SELECT DISTINCT uv FROM UserVocaListEntity uv " +
-            "JOIN FETCH uv.vocaListEntity v " +
-            "JOIN FETCH uv.userEntity u " +
-            "JOIN FETCH VocaContentEntity vc ON vc.vocaListEntity.id = v.id " +  // VocaContentEntity를 패치 조인
-            "WHERE uv.userEntity.email = :email")
+    @Query("SELECT vc from VocaContentEntity vc JOIN fetch vc.vocaListEntity WHERE vc.vocaListEntity.id = :vocaListId")
     List<VocaContentEntity> findByVocaListEntityId(Long vocaListId);
 }
