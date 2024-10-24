24646f91e939f259da30d722d6393cd4c7b1991f
ysm <clov7153@gmail.com>
2024-10-25 00:21:43 +0900

    N+1 문제

--- src/main/java/com/example/vocatest/repository/UserVocaListRepository.java
+++ src/main/java/com/example/vocatest/repository/UserVocaListRepository.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface UserVocaListRepository extends JpaRepository<UserVocaListEntity, Long> {
+    @Query("SELECT uv FROM UserVocaListEntity uv JOIN FETCH uv.vocaListEntity JOIN FETCH uv.userEntity WHERE uv.userEntity.email = :email")
     List<UserVocaListEntity> findByUserEntityEmail(String email); //파라미터로 받는 값이 내가 조회할 속성값
 
     UserVocaListEntity findByVocaListEntityId(Long vocaId);
